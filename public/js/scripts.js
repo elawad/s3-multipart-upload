94bfcf43e0de407c16900c66d5eaab3d66dd6c69
Aymen Elawad <aymen.elawad@turner.com>
2016-10-09 17:02:21 -0400

    use table for file details

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,17 +1,24 @@
 (function () {
-  'use strict';
+  "use strict";
 
   document.getElementById("uploadFile").onchange = function () {
-    var length = this.files.length;
-    // Show file names
-    var names = '';
-    for (var i = 0; i < length; i++) {
-      names += this.files[i].name + '\n';
-    }
-    names = names.trim();
-    document.getElementById("fileList").value = names;
+    // Clear table body rows
+    var table = document.getElementById("fileTable");
+    while(table.rows.length) { table.deleteRow(0); }
 
-    // Toggle button if form has files
+    var length = this.files.length;
+    for (var i = 0; i < length; i++) {
+      // Create table row
+      var row   = table.insertRow();
+      var tdData = createRowData(
+        this.files[i].name, this.files[i].type,
+        formatSize(this.files[i].size)
+      );
+      // Insert row data
+      row.innerHTML = tdData;
+    }
+
+    // Toggle submit button if form has files
     var button = document.getElementById("submitButton");
     if (length > 0) {
       button.removeAttribute("disabled");
@@ -20,13 +27,33 @@
     }
   };
 
-  // Toggle form submit; multi or regular upload
+  // Change form action on toggle; multi or regular upload
   document.getElementById("isMultiPart").onchange = function () {
     var label = document.getElementById("isMultiPart-label");
     var isChecked = !label.classList.contains("is-checked");
     var formAction = isChecked ? "/upload-multi" : "/upload-regular";
 
     document.getElementById("uploadForm").action = formAction;
+  };
+
+  function createRowData(name, type, size) {
+    var strClass = "mdl-data-table__cell--non-numeric";
+    var tdData = `
+      <td class="mdl-data-table__cell--non-numeric">${name}</td>
+      <td class=${strClass}>${type}</td>
+      <td>${size}</td>
+    `;
+    return tdData;
+  };
+
+  function formatSize(size) {
+    var fileSize = '';
+    if (size > 1024 * 1024) {
+      fileSize = (Math.round(size * 100 / (1024 * 1024)) / 100).toString() + ' Mb';
+    } else {
+      fileSize = (Math.round(size * 100 / 1024) / 100).toString() + ' Kb';
+    }
+    return fileSize;
   };
 
 })();
